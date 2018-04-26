97519e6f2f8f09c2dce07bab88fde44a67954bbc
Jack Grahl <jack.grahl@gmail.com>
2018-04-26 22:18:07 +0000

    Use the function to write to a file.

--- A294381.py
+++ A294381.py
@@ -1,3 +1,5 @@
+from utils import write_b_file
+
 def A294381(n):
     a = [1, 3]
     b = [2]
@@ -12,14 +14,5 @@ def A294381(n):
                 b.append(i)
 
 
-def b_file_format(values):
-    i = 0
-    for n in values:
-        yield "%d %d\n" % (i, n)
-        i = i + 1
-
-
 if __name__ == '__main__':
-    with open('A294381.txt', 'w') as f:
-        for line in b_file_format(A294381(50)):
-            f.write(line)
+    write_b_file("data/A294381.txt", A294381(50))
