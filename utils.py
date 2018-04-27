5982852ab5afda2c909bab9dea19fd929bc319e1
Jack Grahl <jack.grahl@gmail.com>
2018-04-27 22:30:37 +0000

    Add some code to output data section format.

--- utils.py
+++ utils.py
@@ -9,3 +9,27 @@ def write_b_file(name, values):
     with open(name, 'w') as f:
         for line in b_file_format(values):
             f.write(line)
+
+
+def chunks(s, n):
+    a = s
+    while len(a) > n:
+        yield a[:n]
+        a = a[n:]
+    yield a
+
+
+def data_section(values):
+    ss = [str(v) for v in values]
+    l = []
+    a = ss[0]
+    c = len(a)
+    for s in ss[1:]:
+        if c + len(s) < 71:
+            a = a + "," + s
+            c = c + len(s) + 1
+        else:
+            l.append(a)
+            a = s
+            c = len(s)
+    return "\n".join(l)
