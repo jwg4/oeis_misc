cdb8ed60bb8ba4f740b2fd127b1b569de87ff16f
Jack Grahl <jack.grahl@gmail.com>
2018-04-29 20:24:03 +0000

    Add code for int(n/e) and related.

--- sequences/simple.py
+++ sequences/simple.py
@@ -1,4 +1,4 @@
-from .basic import nonnegative_integers
+from .basic import naturals, nonnegative_integers
 
 
 def pentagonal_numbers():
@@ -10,3 +10,14 @@ def A000326():
     return pentagonal_numbers()
 
 A000326.start = 0
+
+
+def e_proportion():
+    e = 2.718281828
+    for i in naturals():
+        yield int(i * (1.0 - 1.0 / e))
+
+
+def e_proportion_remainder():
+    for n, i in zip(naturals(), e_proportion()):
+        yield n - i
