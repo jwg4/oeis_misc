54e4dd2c45a07837a66a1458aa2a4f25dd847ea4
Jack Grahl <jack.grahl@gmail.com>
2018-05-05 22:16:08 +0000

    Add the code for A225593.

--- sequences/src/Main.hs
+++ sequences/src/Main.hs
@@ -6,3 +6,15 @@ main = do
 
 naturals :: [Integer]
 naturals = 1 : map (+ 1) naturals
+
+e :: Float
+e = 2.718281828
+
+n_over_e :: [Float]
+n_over_e = map ((/ e) . fromIntegral) naturals
+
+int_n_over_e :: [Integer]
+int_n_over_e = map round n_over_e
+
+A225593 :: [Integer]
+A225593 = int_n_over_e
