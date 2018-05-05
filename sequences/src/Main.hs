33038897b17645313e0db3c63f726b9f42fc87b7
Jack Grahl <jack.grahl@gmail.com>
2018-05-05 22:25:33 +0000

    Do some stuff with n(1 - 1/e)

--- sequences/src/Main.hs
+++ sequences/src/Main.hs
@@ -16,5 +16,8 @@ n_over_e = map ((/ e) . fromIntegral) naturals
 int_n_over_e :: [Integer]
 int_n_over_e = map round n_over_e
 
-A225593 :: [Integer]
-A225593 = int_n_over_e
+a225593 :: [Integer]
+a225593 = int_n_over_e
+
+one_minus_one_over_e_times_n :: [Integer]
+one_minus_one_over_e_times_n = zipWith (-) naturals int_n_over_e
