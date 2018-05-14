7c6ef925413313967b2a551fae4fbd4377ac69ef
Jack Grahl <jack.grahl@gmail.com>
2018-05-14 14:40:45 +0000

    Comment out triangle numbers - doesn't work rn.

--- sequences/src/Main.hs
+++ sequences/src/Main.hs
@@ -28,9 +28,9 @@ square_numbers :: [Integer]
 square_numbers = map square naturals
   where square x = x * x
 
-triangle_numbers :: [Integer]
-triangle_numbers = map triangle naturals
-  where triangle x = toInteger $ x * (x + 1) / 2
+--triangle_numbers :: [Integer]
+--triangle_numbers = map triangle naturals
+--  where triangle (x::Integer) = toInteger $ x * (x + 1) / 2
 
 xor_triangles :: [Int]
 xor_triangles = map (xor_triangle . fromIntegral) naturals
