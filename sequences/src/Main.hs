19ba1740f9b2d42f8de3b673fd4d477006bae648
Jack Grahl <jack.grahl@gmail.com>
2018-05-14 14:41:53 +0000

    Define a sequence of Integers.

--- sequences/src/Main.hs
+++ sequences/src/Main.hs
@@ -35,3 +35,6 @@ square_numbers = map square naturals
 xor_triangles :: [Int]
 xor_triangles = map (xor_triangle . fromIntegral) naturals
   where xor_triangle x = foldl xor 0 $ [1..x]
+
+a003815 :: [Integer]
+a003815 = map fromIntegral xor_triangles
