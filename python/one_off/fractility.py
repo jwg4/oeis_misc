5e565b3d0aefcfc65bfd51ea52fbd027ebdc940b
Jack Grahl <jack@prismfp.com>
2018-06-27 13:59:32 +0100

    Write data sections not b-files.

--- python/one_off/fractility.py
+++ python/one_off/fractility.py
@@ -4,7 +4,7 @@ from sequences.basic import naturals
 from sequences.fractility import harmonic_fractility
 from sequences.transform import indexes_equal_to
 from utils.checking import compare_encyclopedia
-from utils.output import write_b_file
+from utils.output import write_b_file, data_section
 
 
 if __name__ == '__main__':
@@ -12,8 +12,8 @@ if __name__ == '__main__':
 
     A270000 = [harmonic_fractility(n) for n in range(2, 1000)]
     write_b_file("../data/A270000.txt", A270000, 2)
-    for i in range(1, 7):
+    for i in range(4, 7):
         a_number = 269803 + i
         values = indexes_equal_to(A270000, i, start=2)
-        filename = "../data/A%d.txt" % a_number
-        write_b_file(filename, values, 1)
+        print("A%d" % a_number)
+        print(data_section(values))
