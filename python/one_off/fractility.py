2f8c17cdf832dd5ebc01d3088dba3de499c2a6ec
Jack Grahl <jack@prismfp.com>
2018-06-27 11:09:43 +0100

    Change this script to write a b-file.

--- python/one_off/fractility.py
+++ python/one_off/fractility.py
@@ -1,9 +1,14 @@
+import logging
+
 from sequences.basic import naturals
 from sequences.fractility import harmonic_fractility
 from utils.checking import compare_encyclopedia
+from utils.output import write_b_file
 
 
-A270000 = (harmonic_fractility(n) for n in naturals(2))
+A270000 = (harmonic_fractility(n) for n in range(2, 1000))
 
 if __name__ == '__main__':
-    compare_encyclopedia("A270000", A270000)
+    logging.basicConfig(level=logging.INFO)
+
+    write_b_file("../data/A270000.txt", A270000, 2) 
