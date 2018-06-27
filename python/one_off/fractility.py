bc48ddf6b68f53bb76c90972fadff7d4233a8157
Jack Grahl <jack@prismfp.com>
2018-06-27 13:03:05 +0100

    Typo: integer not A-string.

--- python/one_off/fractility.py
+++ python/one_off/fractility.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     A270000 = [harmonic_fractility(n) for n in range(2, 1000)]
     write_b_file("../data/A270000.txt", A270000, 2)
     for i in range(1, 7):
-        a_number = A269803 + i
+        a_number = 269803 + i
         values = indexes_equal_to(A270000, i, start=2)
         filename = "../data/A%d.txt" % a_number
         write_b_file(filename, values, 1)
