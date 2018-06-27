c5778159c317044a452abfca6ab5314c64c6c7ed
Jack Grahl <jack@prismfp.com>
2018-06-27 11:36:08 +0100

    Adapt this script to add values to all the level sequences.

--- python/one_off/fractility.py
+++ python/one_off/fractility.py
@@ -2,13 +2,18 @@ import logging
 
 from sequences.basic import naturals
 from sequences.fractility import harmonic_fractility
+from sequences.transform import indexes_equal_to
 from utils.checking import compare_encyclopedia
 from utils.output import write_b_file
 
 
-A270000 = (harmonic_fractility(n) for n in range(2, 1000))
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    write_b_file("../data/A270000.txt", A270000, 2) 
+    A270000 = [harmonic_fractility(n) for n in range(2, 1000)]
+    write_b_file("../data/A270000.txt", A270000, 2)
+    for i in range(1, 7):
+        a_number = A269803 + i
+        values = indexes_equal_to(A270000, i, start=2)
+        filename = "../data/A%d.txt" % a_number
+        write_b_file(filename, values, 1)
