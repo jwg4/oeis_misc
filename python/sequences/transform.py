a58c0130f3ad546f8c7b43c42936ce1c3203cbaf
Jack Grahl <jack@prismfp.com>
2018-06-28 14:16:59 +0100

    Add a function to prefix a sequence.

--- python/sequences/transform.py
+++ python/sequences/transform.py
@@ -4,3 +4,10 @@ def indexes_equal_to(values, value, start=1):
         if x == value:
             yield i
         i = i + 1
+
+
+def prefix(prefixes, sequence):
+    for i in prefixes:
+        yield i
+    for i in sequence:
+        yield i
