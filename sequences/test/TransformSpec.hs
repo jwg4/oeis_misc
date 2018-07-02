abf5241773ef734d7ce54c40ed1bad2c5117deba
Jack Grahl <jack@prismfp.com>
2018-07-02 15:51:59 +0100

    Add another test for this function.

--- sequences/test/TransformSpec.hs
+++ sequences/test/TransformSpec.hs
@@ -10,3 +10,7 @@ spec = do
     it "trivial intersection" $
         (take 5 (increasing_intersect [1, 2, 3, 4, 5] [1, 2, 3, 4, 5]))
           `shouldBe` [1, 2, 3, 4, 5]
+
+    it "basic intersection" $
+        (take 2 (increasing_intersect [2, 3, 5, 7, 11] [2, 4, 5, 6, 8, 10]))
+          `shouldBe` [2, 5]
