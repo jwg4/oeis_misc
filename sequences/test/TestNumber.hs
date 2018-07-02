19fde5211bf2507f190671cf5c933e503800c824
Jack Grahl <jack@prismfp.com>
2018-07-02 15:22:43 +0100

    Split tests into sequences.

--- sequences/test/TestNumber.hs
+++ sequences/test/TestNumber.hs
@@ -1,4 +1,4 @@
-import Test.Hspec        (Spec, it, shouldBe)
+import Test.Hspec        (Spec, it, shouldBe, describe)
 import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
 
 import Number (primes, divides)
@@ -8,13 +8,14 @@ main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
 specs = do
-
+  describe "primes" $ do
     it "first five primes" $
           (take 5 primes) `shouldBe` [2, 3, 5, 7, 11]
 
     it "hundredth prime" $
         last (take 100 primes) `shouldBe` 541
 
+  describe "divides" $ do
     it "basic divisibility test" $
           (divides 4 2) `shouldBe` True
 
