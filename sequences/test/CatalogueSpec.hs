804bacbc908910ee83837c52908c8b48403dd5a1
Jack Grahl <jack@prismfp.com>
2018-07-02 16:24:23 +0100

    Check values of a003679.

--- sequences/test/CatalogueSpec.hs
+++ sequences/test/CatalogueSpec.hs
@@ -9,6 +9,9 @@ atLeast 0 _ = True
 atLeast _ [] = False
 atLeast n (_:xs) = atLeast (n-1) xs
 
+check :: [Integer] -> [Integer] -> Bool
+check a b = all id $ zipWith (==) a b
+
 spec :: Spec
 spec = do
   describe "a000040" $ do
@@ -18,3 +21,5 @@ spec = do
   describe "a003679" $ do
     it "sequence exists" $
         atLeast 1 a003679
+    it "values from OEIS" $
+        check [4, 8, 9, 16, 19, 20, 21, 26, 30, 31, 33, 38] a003679
