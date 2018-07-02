d20f9c854dac7b34f78930edf61003a3e8752c6a
Jack Grahl <jack@prismfp.com>
2018-07-02 15:07:26 +0100

    Correct the algo to decide where we stop.

--- sequences/src/Number.hs
+++ sequences/src/Number.hs
@@ -3,7 +3,7 @@ module Number where
 import Basic(_sequence)
 
 primes :: [Integer]
-primes = 2 : 3 : filter (\a -> all (not . divides a) (takeWhile (\x -> x < a - 2) primes)) (_sequence 4)
+primes = 2 : 3 : filter (\a -> all (not . divides a) (takeWhile (\x -> x <= a `div` 2) primes)) (_sequence 4)
 
 divides :: Integer -> Integer -> Bool
 divides a b = a `mod` b == 0
