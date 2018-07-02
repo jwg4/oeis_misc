42e29ae918e4cc47a0c4e364eab7f38497a89d07
Jack Grahl <jack@prismfp.com>
2018-07-02 15:12:25 +0100

    Test the hundredth prime.

--- sequences/test/TestNumber.hs
+++ sequences/test/TestNumber.hs
@@ -12,6 +12,9 @@ specs = do
     it "first five primes" $
           (take 5 primes) `shouldBe` [2, 3, 5, 7, 11]
 
+    it "hundredth prime" $
+        last (take 100 primes) `shouldBe` 541
+
     it "basic divisibility test" $
           (divides 4 2) `shouldBe` True
 
