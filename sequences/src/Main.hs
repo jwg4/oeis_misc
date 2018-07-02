6052c61884453b0789fab6902948e1f832c6baa6
Jack Grahl <jack@prismfp.com>
2018-07-02 11:56:31 +0100

    Implement pentagonal numbers.

--- sequences/src/Main.hs
+++ sequences/src/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import Data.Bits(xor)
-import Basic(naturals)
+import Simple(pentagonal_numbers)
 
 main :: IO ()
 main = do
-  putStrLn $ show $ take 50 naturals
+  putStrLn $ show $ take 50 pentagonal_numbers
--- sequences/src/Simple.hs
+++ sequences/src/Simple.hs
@@ -1,9 +1,19 @@
 module Simple where
 
+import Basic(naturals)
+
 square_numbers :: [Integer]
 square_numbers = map square naturals
   where square x = x * x
 
+triangle :: Integer -> Integer
+triangle x = x * (x + 1) `div` 2
+
 triangle_numbers :: [Integer]
 triangle_numbers = map triangle naturals
-  where triangle (x::Integer) = toInteger $ x * (x + 1) / 2
+
+pentagon :: Integer -> Integer
+pentagon x = x * (3 * x - 1) `div` 2
+
+pentagonal_numbers :: [Integer]
+pentagonal_numbers = map pentagon naturals
