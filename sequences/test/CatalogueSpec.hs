ec2a65f1e4abf0e5f4fa2b0f8af5cf67806d512b
Jack Grahl <jack@prismfp.com>
2018-07-02 15:56:26 +0100

    Implement a test to make sure the sequence exists.

--- sequences/test/CatalogueSpec.hs
+++ sequences/test/CatalogueSpec.hs
@@ -4,8 +4,13 @@ import Test.Hspec (Spec, it, shouldBe, describe, pending)
 
 import Catalogue (a000040)
 
+atLeast :: Int -> [a] -> Bool
+atLeast 0 _ = True
+atLeast _ [] = False
+atLeast n (_:xs) = atLeast (n-1) xs
+
 spec :: Spec
 spec = do
   describe "a000040" $ do
     it "sequence exists" $
-        pending
+        atLeast 1 a000040
