b6ac67f742d1178a886ce69a2840d1114e7c83e0
Jack Grahl <jack@prismfp.com>
2018-07-03 11:56:47 +0100

    Add function up_to.

--- sequences/src/Basic.hs
+++ sequences/src/Basic.hs
@@ -9,6 +9,9 @@ naturals = _sequence 1
 a000027 :: [Integer]
 a000027 = naturals
 
+up_to :: Int -> [Integer]
+up_to = (flip take) naturals
+
 nonnegative_integers :: [Integer]
 nonnegative_integers = _sequence 0
 
--- sequences/test/BasicSpec.hs
+++ sequences/test/BasicSpec.hs
@@ -2,7 +2,7 @@ module BasicSpec where
 
 import Test.Hspec (Spec, it, shouldBe, describe)
 
-import Basic (naturals)
+import Basic (naturals, up_to)
 
 spec :: Spec
 spec = do
@@ -12,3 +12,7 @@ spec = do
 
     it "hundredth natural" $
         naturals!!99 `shouldBe` 100
+
+  describe "up_to" $ do
+    it "up_to 10" $ 
+      up_to 10 `shouldBe` [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
