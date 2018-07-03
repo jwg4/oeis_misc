bb2fddbb4b0435e8e1e0fe6339027bc3fe21cb68
Jack Grahl <jack@prismfp.com>
2018-07-03 11:45:38 +0100

    Add a function to filter a given range of integers.

--- sequences/src/Summand.hs
+++ sequences/src/Summand.hs
@@ -3,8 +3,8 @@ module Summand where
 import Basic(naturals)
 import Simple(pentagonal_numbers)
 
-notSumOf :: Integer -> [Integer] -> [Integer]
-notSumOf n summands = filter (not . isSumOf n summands) naturals
+notSumOf :: Integer -> [Integer] -> [Integer] -> [Integer]
+notSumOf n summands = filter (not . isSumOf n summands)
 
 isSumOf :: Integer -> [Integer] -> Integer -> Bool
 isSumOf k sequence n = _isSumOf k (takeWhile (\x -> x <= n) sequence) n 
@@ -13,5 +13,8 @@ _isSumOf :: Integer -> [Integer] -> Integer -> Bool
 _isSumOf 1 sequence n = n `elem` sequence
 _isSumOf k sequence n = any (_isSumOf (k-1) sequence) (map (n -) sequence)
 
+check_a003679 :: [Integer] -> [Integer]
+check_a003679 = notSumOf 3 pentagonal_numbers
+
 a003679 :: [Integer]
-a003679 = notSumOf 3 pentagonal_numbers
+a003679 = check_a003679 naturals
