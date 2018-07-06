2ba7347c0b09c0368e668ef7f8534c83b8c62467
Jack Grahl <jack@prismfp.com>
2018-07-06 11:34:27 +0100

    Attempted optimization.

--- sequences/src/Summand.hs
+++ sequences/src/Summand.hs
@@ -11,7 +11,7 @@ isSumOf k sequence n = _isSumOf k (takeWhile (\x -> x <= n) sequence) n
 
 _isSumOf :: Integer -> [Integer] -> Integer -> Bool
 _isSumOf 1 sequence n = n `elem` sequence
-_isSumOf k sequence n = any (_isSumOf (k-1) sequence) (map (n -) sequence)
+_isSumOf k sequence n = any (isSumOf (k-1) sequence) (map (n -) sequence)
 
 check_a003679 :: [Integer] -> [Integer]
 check_a003679 = notSumOf 3 pentagonal_numbers
