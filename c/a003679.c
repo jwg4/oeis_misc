8f64661bdb2981f2e1cd0a625237a4029763279f
Jack Grahl <jack@prismfp.com>
2018-07-09 11:16:46 +0100

    Start to construct the sieve.

--- c/a003679.c
+++ c/a003679.c
@@ -1,3 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define LIMIT 1000000
+
+
+typedef struct ll{
+    int value;
+    struct ll* next;
+}llT;
+
+llT* append(llT *p, int value) {
+    llT *temp = (llT *)malloc(sizeof(llT));
+    temp->value = value;
+    temp->next = 0;
+    p->next = temp;
+    return temp;
+}
+
+void load_values(llT* list) {
+    int x = 1;
+    list->value = 0;
+    llT* p = list;
+    for (int i=1; x < LIMIT; i++) {
+        x = i * (3 * i - 1) / 2;
+        p = append(p, x);
+    }
+}
+
+void load_pairs(llT* src1, llT* src2, llT* dest) {
+    llT* p = dest;
+    for (llT* s1 = src1; s1->next != 0; s1 = s1->next) {
+        for (llT* s2 = src2; s2->next != 0; s2 = s2->next) {
+            int sum = s1->value + s2->value;
+            p = append(p, sum);
+        }
+    }
+}
 int main(int argc, char** argv) {
+    char sieve[LIMIT];
+    llT* values = (llT*)malloc(sizeof(llT));
+    llT* pairs= (llT*)malloc(sizeof(llT));
+    llT* triplets = (llT*)malloc(sizeof(llT));
+    
+    load_values(values);
+    load_pairs(values, values, pairs);
+
     return 0;
 }
