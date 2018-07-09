3f9178c1504c8dd43040d0b1704ea42ed10ff93e
Jack Grahl <jack@prismfp.com>
2018-07-09 17:12:32 +0100

    A stub for this function.

--- sequences/src/Digit.hs
+++ sequences/src/Digit.hs
@@ -4,3 +4,6 @@ import Data.Char (digitToInt)
 
 digits :: Integer -> [Integer]
 digits = map (fromIntegral . digitToInt) . show
+
+contains :: Integer -> Integer -> Bool
+contains x y = True
--- sequences/test/DigitSpec.hs
+++ sequences/test/DigitSpec.hs
@@ -2,10 +2,15 @@ module DigitSpec where
 
 import Test.Hspec (Spec, it, shouldBe, describe)
 
-import Digit (digits)
+import Digit (digits, contains)
 
 spec :: Spec
 spec = do
   describe "digits" $ do
     it "small input number" $
           digits 12345 `shouldBe` [1, 2, 3, 4, 5]
+
+
+  describe "contains" $ do
+    it "a number contains itself" $
+        contains 45 45 `shouldBe` True
