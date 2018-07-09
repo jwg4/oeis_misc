67f44a101f5ab94c517d809db3b8522247d45216
Jack Grahl <jack@prismfp.com>
2018-07-09 11:56:20 +0100

    Make the size of the sieve into a variable.

--- c/a003679.c
+++ c/a003679.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define LIMIT 1000000
 
 
 typedef struct ll{
@@ -16,11 +15,11 @@ llT* append(llT *p, int value) {
     return temp;
 }
 
-void load_values(llT* list) {
+void load_values(llT* list, int N) {
     int x = 1;
     list->value = 0;
     llT* p = list;
-    for (int i=1; x < LIMIT; i++) {
+    for (int i=1; x < N; i++) {
         x = i * (3 * i - 1) / 2;
         p = append(p, x);
     }
@@ -36,34 +35,35 @@ void load_pairs(llT* src1, llT* src2, llT* dest) {
     }
 }
 
-
-void sieve_pairs(llT* src1, llT* src2, char* dest) {
+void sieve_pairs(llT* src1, llT* src2, char* dest, int N) {
     for (llT* s1 = src1; s1->next != 0; s1 = s1->next) {
         for (llT* s2 = src2; s2->next != 0; s2 = s2->next) {
             int sum = s1->value + s2->value;
-            if (sum < LIMIT) {
+            if (sum < N) {
                 dest[sum] = 1;
             }
         }
     }
 }
 
-void print_values(char* sieve) {
-    for (int i = 0; i < LIMIT; i++) {
+void print_values(char* sieve, int N) {
+    for (int i = 0; i < N; i++) {
         if (sieve[i] == 0) {
             printf("%d\n", i);
         }
     }
 }
+
 int main(int argc, char** argv) {
+    int LIMIT = 1000000;
     char* sieve = (char * )calloc(LIMIT, sizeof(llT));
     llT* values = (llT*)malloc(sizeof(llT));
     llT* pairs= (llT*)malloc(sizeof(llT));
     
-    load_values(values);
+    load_values(values, LIMIT);
     load_pairs(values, values, pairs);
-    sieve_pairs(values, pairs, sieve);
-    print_values(sieve);
+    sieve_pairs(values, pairs, sieve, LIMIT);
+    print_values(sieve, LIMIT);
 
     return 0;
 }
