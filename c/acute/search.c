8f7f6e75fb100bab56bd659f8ef8c29689b15916
Jack Grahl <jack@prismfp.com>
2018-07-11 17:25:50 +0100

    Implement checking functions.

--- c/acute/search.c
+++ c/acute/search.c
@@ -13,8 +13,17 @@ typedef struct node{
 } nodeT;
 
 
+char check_angle(nodeT a, nodeT b, nodeT c) {
+    long ip_w = ((long)a.w - (long)b.w) * ((long)c.w - (long)b.w);
+    long ip_x = ((long)a.x - (long)b.x) * ((long)c.x - (long)b.x);
+    long ip_y = ((long)a.y - (long)b.y) * ((long)c.y - (long)b.y);
+    long ip_z = ((long)a.z - (long)b.z) * ((long)c.z - (long)b.x);
+    long sum = ip_w + ip_x + ip_y + ip_z;
+    return (sum > 0);
+}
+
 char check_triplet(nodeT a, nodeT b, nodeT c) {
-    return 1;
+    return check_angle(a, b, c) && check_angle(b, c, a) && check_angle(c, a, b);
 }
 
 char check_new_point(nodeT new_point, nodeT* points, int count) {
