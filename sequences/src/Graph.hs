9c41717cc4f74c503998aca5e036ad8f5f60ef41
Jack Grahl <jack@prismfp.com>
2018-07-17 08:54:18 +0100

    Change the number of the sequence.
    A polynomial is a mapping.

--- sequences/src/Graph.hs
+++ sequences/src/Graph.hs
@@ -2,5 +2,8 @@ module Graph where
 
 import Basic (naturals)
 
-claw_chromatic_polynomial :: [Integer]
-claw_chromatic_polynomial = map (\x -> x * (x-1)^3) naturals
+claw_chromatic_polynomial :: Integer -> Integer
+claw_chromatic_polynomial x = x * (x - 1) ^ 3
+
+claw_chromatic_numbers :: [Integer]
+claw_chromatic_numbers = map claw_chromatic_polynomial naturals
