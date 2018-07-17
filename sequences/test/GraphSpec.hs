71111472afacda1be810a8b5112baea5c3f73247
Jack Grahl <jack@prismfp.com>
2018-07-17 08:54:18 +0100

    Change the number of the sequence.
    A polynomial is a mapping.

--- sequences/test/GraphSpec.hs
+++ sequences/test/GraphSpec.hs
@@ -6,6 +6,6 @@ import Graph (claw_chromatic_polynomial)
 
 spec :: Spec
 spec = do
-  describe "claw_chromatic_polynomial" $ do
+  describe "claw_chromatic_numbers" $ do
     it "first five elements" $
-          (take 5 claw_chromatic_polynomial) `shouldBe` [0, 1, 24, 108, 320]
+          (take 5 claw_chromatic_numbers) `shouldBe` [0, 2, 24, 108, 320]
