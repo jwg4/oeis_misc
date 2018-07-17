2dd8a79242451f1b269d02aff800ba36e428a624
Jack Grahl <jack@prismfp.com>
2018-07-17 08:55:19 +0100

    Add an alternative method given in the encylopedia.

--- sequences/test/GraphSpec.hs
+++ sequences/test/GraphSpec.hs
@@ -2,10 +2,14 @@ module GraphSpec where
 
 import Test.Hspec (Spec, it, shouldBe, describe)
 
-import Graph (claw_chromatic_polynomial)
+import Graph (a179824, a179824_alt, claw_chromatic_numbers)
 
 spec :: Spec
 spec = do
   describe "claw_chromatic_numbers" $ do
     it "first five elements" $
           (take 5 claw_chromatic_numbers) `shouldBe` [0, 2, 24, 108, 320]
+
+  describe "A179824" $ do
+    it "compare two methods of calculation" $
+        (take 5 a179824) `shouldBe` (take 5 a179824_alt)
