86749f03d35d238df0a29d2d1a7037a343ccc20c
Jack Grahl <jack@prismfp.com>
2018-07-17 08:55:19 +0100

    Add an alternative method given in the encylopedia.

--- sequences/src/Graph.hs
+++ sequences/src/Graph.hs
@@ -10,3 +10,9 @@ claw_chromatic_numbers = map claw_chromatic_polynomial naturals
 
 a179824 :: [Integer]
 a179824 = drop 1 claw_chromatic_numbers
+
+a179824_alt :: [Integer]
+a179824_alt = map fromIntegral a179824_int
+
+a179824_int :: [Int]
+a179824_int = let f n = [ (x, a, b, c) | let t = [1..n], x <- t, a <- t, x /= a, b <- t, x /= b, c <- t, x /= c ] in map (length . f) [2..]
