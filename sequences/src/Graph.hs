e703498ca870cd37814f168db2fece0960e42825
Jack Grahl <jack@prismfp.com>
2018-07-17 08:54:54 +0100

    Formalize sequence A179824.

--- sequences/src/Graph.hs
+++ sequences/src/Graph.hs
@@ -7,3 +7,6 @@ claw_chromatic_polynomial x = x * (x - 1) ^ 3
 
 claw_chromatic_numbers :: [Integer]
 claw_chromatic_numbers = map claw_chromatic_polynomial naturals
+
+a179824 :: [Integer]
+a179824 = drop 1 claw_chromatic_numbers
