9671192bd8e38bfa5a5a8bbc8865c660387e2aa6
Jack Grahl <jack@prismfp.com>
2018-07-23 10:07:22 +0100

    Implement A316343.

--- sequences/src/Morphism.hs
+++ sequences/src/Morphism.hs
@@ -1,5 +1,19 @@
 module Morphism where
 
+import Basic (naturals)
+
+a316343_count :: Integer -> Int
+a316343_count n = fromIntegral ((v + 1) ^ 2) where
+    v
+        | n `mod` 2 == 1 = 0
+        | otherwise      = 1 + (n `div` 2)
+
+a316343_S :: Integer -> [Integer]
+a316343_S n = 0 : (replicate (a316343_count n) 1)
+
+a316343 :: [Integer]
+a316343 = concatMap a316343_S naturals
+
 generate_sequence :: (Integer -> [Integer]) -> Integer -> [Integer]
 generate_sequence f n = concat $ map snd $ iterate (map_truncate f) ([], [n])
 
