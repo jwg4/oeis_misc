f6aaa0b9e812b66565b084cf6665f1d2e40adff7
Jack Grahl <jack@prismfp.com>
2018-07-30 11:49:21 +0100

    A function to expand the tree of transforms.

--- sequences/test/ComplexSpec.hs
+++ sequences/test/ComplexSpec.hs
@@ -2,7 +2,7 @@ module ComplexSpec where
 
 import Test.Hspec (Spec, it, shouldBe, describe)
 
-import Complex (GaussianRational(GR_BCD), gr_increment, gr_reciprocal, gr_times_i)
+import Complex (GaussianRational(GR_BCD), gr_increment, gr_reciprocal, gr_times_i, all_tree_values)
 
 spec :: Spec
 spec = do
@@ -16,3 +16,10 @@ spec = do
 
     it "reciprocal of i" $
           (gr_reciprocal (GR_BCD 0 1 1)) `shouldBe` (GR_BCD 0 (-1) 1)
+
+  describe "tree expansion" $ do
+    it "Expand 2 and i" $ 
+        all_tree_values [GR_BCD 2 0 1, GR_BCD 0 1 1] `shouldBe` [
+            GR_BCD 3 0 1, GR_BCD 1 0 2, GR_BCD 0 2 1,
+            GR_BCD 1 1 1, GR_BCD 0 (-1) 1, GR_BCD (-1) 0 1 ]
+
