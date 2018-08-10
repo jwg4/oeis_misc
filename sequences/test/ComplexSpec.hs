72ce97c36b4c5c7146a5c2884335815401412b7f
Jack Grahl <jack@prismfp.com>
2018-08-10 10:50:28 +0100

    Clean up code for this sequence.

--- sequences/test/ComplexSpec.hs
+++ sequences/test/ComplexSpec.hs
@@ -2,7 +2,7 @@ module ComplexSpec where
 
 import Test.Hspec (Spec, it, shouldBe, describe)
 
-import Complex (GaussianRational(GR_BCD), gr_increment, gr_reciprocal, gr_times_i, all_tree_values, find_a233695_value)
+import Complex (GaussianRational(GR_BCD), gr_increment, gr_reciprocal, gr_times_i, all_tree_values)
 
 spec :: Spec
 spec = do
@@ -25,10 +25,6 @@ spec = do
         all_tree_values [GR_BCD 2 0 1, GR_BCD 0 1 1] `shouldBe` [
             GR_BCD 3 0 1, GR_BCD 1 0 2, GR_BCD 0 2 1,
             GR_BCD 1 1 1, GR_BCD 0 (-1) 1, GR_BCD (-1) 0 1 ]
-
-  describe "find values" $ do
-    it "Find 1" $ 
-        find_a233695_value 1 `shouldBe` 10
 
   describe "show GaussianRational" $ do
     it "i" $
