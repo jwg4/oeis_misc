e14d6301a3af55556e3b3c06cfc4309991f7c47b
Jack Grahl <jack.grahl@gmail.com>
2022-10-11 10:16:38 +0100

    Implement A113912._

--- python/sequences/A007001.py
+++ python/sequences/A007001.py
@@ -15,6 +15,15 @@ def a007001():
         l = list(expand(l))
 
 
+def a133912():
+    c = 0
+    started = False
+    for j in a007001():
+        yield j - c
+        c = j
+
+
 if __name__ == '__main__':
-    for n in a007001():
-        print(n)
+    from utils.output import data_section, truncate
+
+    print(data_section(truncate(a133912(), 300)))
